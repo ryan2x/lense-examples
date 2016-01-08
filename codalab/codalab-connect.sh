d9d282cfb549fb7349df8ca41688182532a8331a
Keenon Werling <keenon@stanford.edu>
2016-01-08 11:22:43 -0800

    Modifying codalab related files.

---
+++ codalab/codalab-connect.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cl work nlp::
--- codalab/run-all-experiments.sh
+++ codalab/run-all-experiments.sh
@@ -8,45 +8,45 @@ FLAGS="-Xmx32G"
 ######## NER
 
 # Human only (Max vote)
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.ner.runnables.MaxVoteNoMLRealData ner-src 2> ner-dst/max-vote-results.txt
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.ner.runnables.MaxVoteNoMLRealData ner-src 2> ner-dst/max-vote-results.txt
 
 # ML only
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.ner.runnables.NERMLOnlyRealData ner-src ner-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.ner.runnables.NERMLOnlyRealData ner-src ner-dst
 
 # Threshold
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.ner.runnables.NERThresholdGameplayerRealData ner-src ner-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.ner.runnables.NERThresholdGameplayerRealData ner-src ner-dst
 
 # MCTS
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.ner.runnables.NERMCTSGameplayerRealData ner-src ner-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.ner.runnables.NERMCTSGameplayerRealData ner-src ner-dst
 
 ######## Sentiment
 
 # Human only (Max vote)
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.sentiment.runnables.MaxVote sentiment-src 2> sentiment-dst/max-vote-results.txt
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.sentiment.runnables.MaxVote sentiment-src 2> sentiment-dst/max-vote-results.txt
 
 # ML only
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentMLOnly sentiment-src sentiment-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentMLOnly sentiment-src sentiment-dst
 
 # Threshold
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentThresholdGameplayer sentiment-src sentiment-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentThresholdGameplayer sentiment-src sentiment-dst
 
 # MCTS - no embeddings
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentMCTSNoEmbeddingsGameplayer sentiment-src sentiment-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentMCTSNoEmbeddingsGameplayer sentiment-src sentiment-dst
 
 # MCTS
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentMCTSGameplayer sentiment-src sentiment-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.sentiment.runnables.SentimentMCTSGameplayer sentiment-src sentiment-dst
 
 ######## Faces
 
 # Human only (Max vote)
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.faces.runnables.MaxVote person_recognition-src 2> person_recognition-dst/max-vote-results.txt
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.faces.runnables.MaxVote person_recognition-src 2> person_recognition-dst/max-vote-results.txt
 
 # ML only
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.faces.runnables.FacesMLOnly person_recognition-src person_recognition-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.faces.runnables.FacesMLOnly person_recognition-src person_recognition-dst
 
 # Threshold
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.faces.runnables.FacesThresholdGameplay person_recognition-src person_recognition-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.faces.runnables.FacesThresholdGameplay person_recognition-src person_recognition-dst
 
 # MCTS
-java $FLAGS -cp lense-examples-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.keenon.lense.examples.faces.runnables.FacesMCTSGameplay person_recognition-src person_recognition-dst
+java $FLAGS -cp lense.jar com.github.keenon.lense.examples.faces.runnables.FacesMCTSGameplay person_recognition-src person_recognition-dst
 
